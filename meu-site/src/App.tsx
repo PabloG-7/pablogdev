c66cf212b8c116d9cc4e0a97f749c056b4955f1b
PabloG-7 <pablooliver853@gmail.com>
2026-09-15 18:26:54 -0300

    update

--- meu-site/src/App.tsx
+++ meu-site/src/App.tsx
@@ -42,8 +42,8 @@ function App() {
       </section>
 
       <section id="about">
-        <About />
-      </section>
+  {/* <About /> */}
+</section>
 
       <section id="process">
         <Process />
